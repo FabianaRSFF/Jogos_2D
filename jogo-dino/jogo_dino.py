88efda75f0c3a215ddd0a001f004abc57dfa48f5
fabianarsff <fabianarsantos@gmail.com>
2023-05-27 14:13:02 -0300

    adicionando pontuação

--- jogo-dino/jogo_dino.py
+++ jogo-dino/jogo_dino.py
@@ -24,8 +24,23 @@ imagens_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpriteshe
 
 som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'death_sound.wav'))  # noqa E501
 som_colisao.set_volume(1)
+
+som_pontuacao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'score_sound.wav'))   # noqa E501
+som_pontuacao.set_volume(1)
+
 colidiu = False
+
 escolha_obstaculo = choice([0, 1])
+pontos = 0
+
+velocidade_jogo = 10
+
+
+def exibe_mensagem(msg, tam_fonte, cor):
+    fonte = pygame.font.SysFont('comicsansms', tam_fonte, True, False)
+    mensagem = f'{msg}'
+    texto_formatado = fonte.render(mensagem, True, cor)
+    return texto_formatado
 
 
 class Dino(pygame.sprite.Sprite):
@@ -80,7 +95,7 @@ class Nuvens(pygame.sprite.Sprite):
         if self.rect.topright[0] < 0:
             self.rect.x = largura
             self.rect.y = randrange(50, 200, 50)
-        self.rect.x -= 10
+        self.rect.x -= velocidade_jogo
 
 
 class Chao(pygame.sprite.Sprite):
@@ -112,7 +127,7 @@ class Cactus(pygame.sprite.Sprite):
         if self.escolha == 0:
             if self.rect.topright[0] < 0:
                 self.rect.x = largura
-            self.rect.x -= 10
+            self.rect.x -= velocidade_jogo
 
 
 class DinoVoador(pygame.sprite.Sprite):
@@ -136,7 +151,7 @@ class DinoVoador(pygame.sprite.Sprite):
         if self.escolha == 1:
             if self.rect.topright[0] < 0:
                 self.rect.x = largura
-            self.rect.x -= 10
+            self.rect.x -= velocidade_jogo
 
             if self.index_lista > 1:
                 self.index_lista = 0
@@ -197,8 +212,20 @@ while True:
         som_colisao.play()
         colidiu = True
     if colidiu == True:
+        if pontos % 100 == 0:
+            pontos += 1
         pass
     else:
+        pontos += 1
         todas_as_sprites.update()
+        texto_pontos = exibe_mensagem(pontos, 40, (0, 0, 0))
 
+    if pontos % 100 == 0:
+        som_pontuacao.play()
+        if velocidade_jogo >= 23:
+            velocidade_jogo += 0
+        else:
+            velocidade_jogo += 1
+    
+    tela.blit(texto_pontos, (520, 30))
     pygame.display.flip()
